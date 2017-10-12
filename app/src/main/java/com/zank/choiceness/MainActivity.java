b5f2b157cb56a69fde39a715f61806d4e7283f8a
Zank <741256459@qq.com>
2017-10-12 17:32:37 +0800

    perfect Splash

--- app/src/main/java/com/zank/choiceness/MainActivity.java
+++ app/src/main/java/com/zank/choiceness/MainActivity.java
@@ -1,13 +1,38 @@
 package com.zank.choiceness;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.zank.choiceness.base.BaseActivity;
+import com.zank.choiceness.base.IBaseView;
+
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int attachLayoutRes() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initInjector() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateViews(boolean isRefresh) {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
